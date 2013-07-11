b5f42dc24db7266d8be8ab613fca1a743ea0b4ad
Ben Shope <nimajnebs@gmail.com>
2013-07-11 15:32:23 -0700

    Fab-update the app

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-#!/usr/bin/env python
-
-import os
-import jinja2
 import webapp2
+import jinja2
+import os
 
 jinja = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'])
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/html"))
 
-
-class MainPage(webapp2.RequestHandler):
-
+class HomePage(webapp2.RequestHandler):
     def get(self):
-
-        template_values = {}
-        template = jinja.get_template('index.html')
-        self.response.write(template.render(template_values))
-
+        template = jinja.get_template('home.html')
+        self.response.write(template.render({}))
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', HomePage)
 ], debug=True)
